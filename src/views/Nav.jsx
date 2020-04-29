3d973fbe922ef0acac36f0cd066187b62d22cd2e
Safiamoon <msellek.safia@gmail.com>
2020-04-29 14:35:19 +0200

    Step1-Header : Updated navbar

--- src/views/Nav.jsx
+++ src/views/Nav.jsx
@@ -6,39 +6,39 @@ import "./NavStyle.css";
 
 const Nav = () => {
   return (
-    <nav class="navbar navbar-menu" role="navigation">
+    <nav className="navbar navbar-menu" role="navigation">
 
-      <div class="md-stepper-horizontal orange">
+      <div className="md-stepper-horizontal orange">
 
-        <div class="md-step active done">
-            <div class="md-step-circle"><span>1</span></div>
-            <Link to="/"><div class="md-step-title">Home</div></Link>
-            <div class="md-step-bar-left"></div>
-            <div class="md-step-bar-right"></div>
+        <div className="md-step active done">
+            <div className="md-step-circle"><span>1</span></div>
+            <Link to="/"><div className="md-step-title">Home</div></Link>
+            <div className="md-step-bar-left"></div>
+            <div className="md-step-bar-right"></div>
         </div>
 
-        <div class="md-step active">
-            <div class="md-step-circle"><span>1</span></div>
-            <Link to="/rythme"><div class="md-step-title">Rythme</div></Link>
-            <div class="sous-titre">Choose your note and create your own music partition</div>
-            <div class="md-step-bar-left"></div>
-            <div class="md-step-bar-right"></div>
+        <div className="md-step active">
+            <div className="md-step-circle"><span>1</span></div>
+            <Link to="/rythme"><div className="md-step-title">Rythme</div></Link>
+            <div className="sous-titre">Choose your note and create your own music partition</div>
+            <div className="md-step-bar-left"></div>
+            <div className="md-step-bar-right"></div>
         </div>
 
-        <div class="md-step active">
-            <div class="md-step-circle"><span>2</span></div>
-            <Link to="/percussions"><div class="md-step-title">Learn</div></Link>
-            <div class="sous-titre" >Associate the note with a part of the body</div>
-            <div class="md-step-bar-left"></div>
-            <div class="md-step-bar-right"></div>
+        <div className="md-step active">
+            <div className="md-step-circle"><span>2</span></div>
+            <Link to="/percussions"><div className="md-step-title">Learn</div></Link>
+            <div className="sous-titre" >Associate the note with a part of the body</div>
+            <div className="md-step-bar-left"></div>
+            <div className="md-step-bar-right"></div>
         </div>
 
-        <div class="md-step active">
-            <div class="md-step-circle"><span>3</span></div>
-            <Link to=""><div class="md-step-title">Percussion</div></Link>
-            <div class="sous-titre" >Listen to the percussion that you made</div>
-            <div class="md-step-bar-left"></div>
-            <div class="md-step-bar-right"></div>
+        <div className="md-step active">
+            <div className="md-step-circle"><span>3</span></div>
+            <Link to=""><div className="md-step-title">Percussion</div></Link>
+            <div className="sous-titre" >Listen to the percussion that you made</div>
+            <div className="md-step-bar-left"></div>
+            <div className="md-step-bar-right"></div>
         </div>
 
       </div>
