f8358eb2d5da8af1a73e9be202d58caaa23d76f2
Safiamoon <msellek.safia@gmail.com>
2020-04-29 16:59:12 +0200

    Step-1-Header Navbar in progress

--- src/views/Nav.jsx
+++ src/views/Nav.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router, Switch, Link } from "react-router-dom";
+import { Router, Switch, NavLink } from "react-router-dom";
 import "./NavStyle.css";
 
 const Nav = () => {
@@ -10,14 +10,14 @@ const Nav = () => {
 
         <div className="md-step active done">
             <div className="md-step-circle"><span>1</span></div>
-            <Link to="/"><div className="md-step-title">Home</div></Link>
+            <NavLink activeclassName="selected" to="/"><div className="md-step-title">Home</div></NavLink>
             <div className="md-step-bar-left"></div>
             <div className="md-step-bar-right"></div>
         </div>
 
         <div className="md-step active">
             <div className="md-step-circle"><span>1</span></div>
-            <Link to="/rythme"><div className="md-step-title">Rythme</div></Link>
+            <NavLink activeclassName="selected" to="/rythme"><div className="md-step-title">Rythme</div></NavLink>
             <div className="sous-titre">Choose your note and create your own music partition</div>
             <div className="md-step-bar-left"></div>
             <div className="md-step-bar-right"></div>
@@ -25,7 +25,7 @@ const Nav = () => {
 
         <div className="md-step active">
             <div className="md-step-circle"><span>2</span></div>
-            <Link to="/percussions"><div className="md-step-title">Learn</div></Link>
+            <NavLink activeclassName="selected" to="/percussions"><div className="md-step-title">Learn</div></NavLink>
             <div className="sous-titre" >Associate the note with a part of the body</div>
             <div className="md-step-bar-left"></div>
             <div className="md-step-bar-right"></div>
@@ -33,7 +33,7 @@ const Nav = () => {
 
         <div className="md-step active">
             <div className="md-step-circle"><span>3</span></div>
-            <Link to=""><div className="md-step-title">Percussion</div></Link>
+            <NavLink activeclassName="selected" to=""><div className="md-step-title">Percussion</div></NavLink>
             <div className="sous-titre" >Listen to the percussion that you made</div>
             <div className="md-step-bar-left"></div>
             <div className="md-step-bar-right"></div>
