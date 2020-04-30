6cae4a048f58b56c9ef211e9b0e5aa658d8a4a93
sunsay_dev <sunsay.dev@gmail.com>
2020-04-30 16:56:04 +0200

    fix: old deleted services imports

--- src/components/step2/Partition.jsx
+++ src/components/step2/Partition.jsx
@@ -1,6 +1,4 @@
 import React, { useContext } from "react";
-//services
-import { addNoteToDom } from "../../services/staveService";
 //context
 import PartitionContext from "../../context/PartitionContext";
 //components
@@ -14,13 +12,7 @@ const Partition = () => {
   const userComposition = partitionContext.notes;
 
   userComposition.map((note) => {
-    addNoteToDom(
-      note,
-      partitionContext.notes,
-      note.image,
-      partitionContext.allNotesWidth,
-      partitionContext.setAllNotesWidth
-    );
+   
 
     return null;
   });
